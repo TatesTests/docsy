f8da844b35138035d92f8ba5555d74c307ef1940
TatesTests <84292212+TatesTests@users.noreply.github.com>
2022-04-19 12:40:00 +1200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+console.log(module.paths)
+console.log(process.env);
+
 module.exports = {
     plugins: {
         autoprefixer: {}
