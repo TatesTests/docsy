cef9ca5f1489497fb681674fc392de6df9c0f7fd
TatesTests <84292212+TatesTests@users.noreply.github.com>
2022-04-19 12:49:43 +1200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-console.log(module.paths)
-console.log(process.env);
+console.error(module.paths)
+console.error(process.env);
 
 module.exports = {
     plugins: {
